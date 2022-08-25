1dc928e6c26201efc558fad4f27d9266f9cd56eb
Luan7u7 <luanhenriquevg@gmail.com>
2022-08-25 13:45:12 -0300

    menu lateral
    troquei o aside por uma div com id="barramenu" e coloquei função de click com .js

---
+++ script.js
@@ -0,0 +1,9 @@
+function clickmenu(){
+    if (barramenu.style.display == 'block'){
+        barramenu.style.display = 'none'
+    }
+    else {
+        barramenu.style.display = 'block'
+    }
+
+}
